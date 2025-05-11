4879ce79c91a5cdb76abc851462a58dd781303c9
Aplace <aplace030927@gmail.com>
2025-05-11 06:09:10 +0000

    savepoint

---
+++ src/build/main_libarchive.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+import subprocess
+import os
+import logging
+import time
+
+MAX_CPU_PER_JOB = 8
+
+
+def read_commit_hashes(target) -> list[str]:
+    with open(f"./compare/{target}/compares", "r") as f:
+        result = [line for line in f.read().splitlines()]
+    return result
+
+
+def serial_jobs_configure(
+    cmds: list[tuple[list[str], Optional[str], bool]],
+) -> tuple[int, Optional[str]]:
+    for cmd, cwd, check in cmds:
+        start = time.time()
+        proc = subprocess.run(cmd, cwd=cwd, stdout=subprocess.DEVNULL)
+        elapsed = time.time() - start
+
+        if not check and proc.returncode:
+            # discord.add_message_to_last_field(f"* :warning: `{' '.join(cmd)}` *returned {proc.returncode}* ({elapsed:.2f} s)")
+            pass
+        elif check and proc.returncode:
+            # logging.log(logging.ERROR, f"[{' '.join(cmd)}] @ {cwd} returns {proc.returncode}")
+            return (proc.returncode, "".join(cmd) + "\n" + proc.stderr)
+        else:
+            # logging.log(logging.WARN, f"[{' '.join(cmd)}] @ {cwd} exits successfully")
+            # discord.add_message_to_last_field(f"* :white_check_mark: `{' '.join(cmd)}` ({elapsed:.2f} s)")
+            pass
+
+    return (proc.returncode, None)
+
+
+def ir_build(target: str, chash: str) -> tuple[int, Optional[str]]:
+    # logging.log(logging.WARN, f"Start building CHASH={chash}")
+    cleanup()
+    # discord.add_field(f":information_source: Generating LLVM BC of `{chash}`")
+    return serial_jobs_configure(
+        [
+            (["git", "checkout", chash], CONFIG["LIBARCHIVE_DIRECTORY"], True),
+            (
+                # Neutralizing -Werrors. Caution!
+                # find . -type f -exec sed -i 's/-Werror//g' {} +
+                [
+                    "find",
+                    ".",
+                    "-type",
+                    "f",
+                    "-exec",
+                    "sed",
+                    "-i",
+                    "s/-Werror//g",
+                    "{}",
+                    "+",
+                ],
+                CONFIG["LIBARCHIVE_DIRECTORY"],
+                True,
+            ),
+            (
+                [
+                    "awk",
+                    f'{{ print >> "{CONFIG["LIBARCHIVE_DIRECTORY"] + "/libarchive/CMakeLists.txt"}" }}',
+                    "mod_cmakelists.txt",
+                ],
+                CONFIG["CFGDIFF_IRCONV_DIRECTORY"],
+                True,
+            ),
+            (
+                ["cmake", "."],
+                CONFIG["LIBARCHIVE_DIRECTORY"],
+                True,
+            ),
+            (
+                ["make"],
+                CONFIG["LIBARCHIVE_DIRECTORY"],
+                True,
+            ),
+        ]
+    )
+
+
+def make_cfg(target: str, chash: str) -> tuple[int, Optional[str]]:
+    return serial_jobs_configure(
+        [
+            (
+                [
+                    "find",
+                    ".",
+                    "-type",
+                    "f",
+                    "-name",
+                    "*.bc",
+                    "-exec",
+                    "opt",
+                    "-passes=dot-cfg",
+                    "-disable-output",
+                    "{}",
+                    ";",
+                ],
+                CONFIG["BUILD_OUTPUT_DIRECTORY"] + f"/{target}/libarchive-bcs-{chash}",
+                True,
+            ),  # Convert to CFG
+            (
+                ["find", ".", "-type", "f", "-name", "*.bc", "-delete"],
+                CONFIG["BUILD_OUTPUT_DIRECTORY"] + f"/{target}/libarchive-bcs-{chash}",
+                True,
+            ),  # Remove all bitcodes
+            (
+                [
+                    "find",
+                    ".",
+                    "-type",
+                    "f",
+                    "-name",
+                    "'.*'",
+                    "-exec",
+                    "rename",
+                    "'s|/\.([^/]*)$|/\\1|'",
+                    "{}",
+                    "+",
+                ],
+                CONFIG["BUILD_OUTPUT_DIRECTORY"] + f"/{target}/libarchive-bcs-{chash}",
+                True,
+            ),  # Remove all un-normalized CFGs
+        ]
+    )
+
+
+def cleanup():
+    return serial_jobs_configure(
+        [
+            (["git", "clean", "-fd"], CONFIG["LIBARCHIVE_DIRECTORY"], True),
+            (["git", "reset", "--hard"], CONFIG["LIBARCHIVE_DIRECTORY"], True),
+        ]
+    )
+
+
+if __name__ == "__main__":
+
+    with open(".setup", "r") as setup:
+        CONFIG = {
+            line.split()[0].strip(): line.split()[-1].strip()
+            for line in setup.readlines()
+        }
+
+    for key in CONFIG:
+        os.environ[key] = CONFIG[key]
+
+    target_name = "libarchive"
+
+    commit_hashes = read_commit_hashes(target_name)
+
+    serial_jobs_configure(
+        [(["mkdir", f"{target_name}"], CONFIG["BUILD_OUTPUT_DIRECTORY"], False)]
+    )
+
+    for commit in commit_hashes:
+        print("IR BUILDING")
+        ir_build(target_name, commit)
+        print("CFG BUILDING")
+        make_cfg(target_name, commit)
+
+        # webhook_send.success_webhook([f"Success Diff {vulnerable} {patched}"])
---
+++ src/convert/main_libarchive.py
@@ -0,0 +1,172 @@
+import os
+import subprocess
+import json
+import itertools
+import sys
+from itertools import pairwise
+from colorama import Fore, Back, Style
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+import src.graph.topology as topology
+import src.visual.diffview as diffview
+
+TARGET = "libarchive"
+
+
+def file_diff(f_new: str, f_old: str) -> bool:
+    h_new = subprocess.run(["sha256sum", f_new], capture_output=True).stdout.split()[0]
+    h_old = subprocess.run(["sha256sum", f_old], capture_output=True).stdout.split()[0]
+    return h_new.split()[0] == h_old.split()[0]
+
+
+def setup_env() -> dict[str, str]:
+    with open(".setup", "r") as setup:
+        config = {
+            line.split()[0].strip(): line.split()[-1].strip()
+            for line in setup.readlines()
+        }
+
+        for key in config:
+            os.environ[key] = config[key]
+    return config
+
+
+def git_checkout_to_hash(bid: str):
+    basedir = os.environ.get("LIBARCHIVE_DIRECTORY")
+    subprocess.run(["git", "checkout", bid], cwd=basedir)
+
+
+def check_file_commit_hash(fname: str):
+    basedir = os.environ.get("LIBARCHIVE_DIRECTORY")
+    file = basedir + "/" + fname
+    proc = subprocess.run(
+        ["git", "log", '--format="%H"', file], cwd=basedir, capture_output=True
+    )
+
+    return proc.stdout.decode().replace('"', "").splitlines()
+
+
+def fetch_symbols_from_file(fname: str) -> list[str]:
+    basedir = os.environ.get("LIBARCHIVE_DIRECTORY")
+    file = basedir + "/" + fname
+
+    proc = subprocess.run(
+        ["ctags", "--languages=C", "--kinds-C=f", "--output-format=json", file],
+        cwd=basedir,
+        capture_output=True,
+    )
+
+    return [
+        json.loads(func_info).get("name")
+        for func_info in proc.stdout.decode().splitlines()
+    ]
+
+
+if __name__ == "__main__":
+    setup_env()
+
+    with open(f"compare/{TARGET}/compares_target.json", "r") as f:
+        comp = json.load(f)
+
+    v_new = comp[0]
+    new_hash, new_fn = v_new["hash"], set(v_new["symbol"])
+    new_built_set = set(
+        [
+            fname[:-4]
+            for fname in os.listdir(f"build_output/{TARGET}/libarchive-bcs-{new_hash}/")
+        ]
+    )
+
+    for v_old in comp[1:]:
+
+        # --Suggestion.
+        # Get a "Patch" with v[0] and v[1], and
+        # Compare the (v[0], {v[2], v[3] ... v[n]}) to detect the diff-ed vulnerability
+
+        # v_new = comp[0]
+        # for v_old in comp[1:]:
+
+        old_hash, old_fn = v_old["hash"], set(v_old["symbol"])
+
+        old_built_set = set(
+            [
+                fname[:-4]
+                for fname in os.listdir(
+                    f"build_output/{TARGET}/libarchive-bcs-{old_hash}/"
+                )
+            ]
+        )
+
+        new_fn &= new_built_set
+        old_fn &= old_built_set
+
+        fn_only_new = new_fn - old_fn
+        fn_only_old = old_fn - new_fn
+        fn_intersect = new_fn & old_fn
+
+        hash_same, hash_diff = [], []
+
+        for f in fn_intersect:
+            Gn = topology.build_cfg_from_dot(
+                f"build_output/{TARGET}/libarchive-bcs-{new_hash}/{f}.dot"
+            )
+            Go = topology.build_cfg_from_dot(
+                f"build_output/{TARGET}/libarchive-bcs-{old_hash}/{f}.dot"
+            )
+
+            (v_same, v_diff, v_addr_matching, e_con, e_old, e_new) = (
+                topology.graph_isomorphism(Go, Gn)
+            )
+
+            if v_diff == [] and e_old == [] and e_new == []:
+                continue
+
+            print(
+                f"{Style.BRIGHT}{Back.RED}{f} @ {old_hash}{Style.RESET_ALL} vs\n{Style.BRIGHT}{Back.GREEN}{f} @ {new_hash}{Style.RESET_ALL}"
+            )
+
+            if v_diff != []:
+                for v_old, v_new in v_diff:
+                    if v_old.llvm_ir != []:
+                        print(
+                            Fore.RED
+                            + "- [\n\t"
+                            + ";\n- \t".join(v_old.llvm_ir)
+                            + "\n- ]"
+                        )
+                    if v_new.llvm_ir != []:
+                        print(
+                            Fore.GREEN
+                            + "+ [\n\t"
+                            + ";\n+ \t".join(v_new.llvm_ir)
+                            + "\n+ ]\n"
+                        )
+
+            for edge in e_old:
+                v_src = Go.find_vertex_by_addr(edge.src)
+                v_dst = Go.find_vertex_by_addr(edge.dst)
+                print(
+                    Fore.RED
+                    + f"- {edge.label}:\n- {v_src.llvm_ir_optype} ->\n- {v_dst.llvm_ir_optype}\n"
+                )
+
+            for edge in e_new:
+                v_src = Gn.find_vertex_by_addr(edge.src)
+                v_dst = Gn.find_vertex_by_addr(edge.dst)
+                print(
+                    Fore.GREEN
+                    + f"+ {edge.label}:\n+ {v_src.llvm_ir_optype} ->\n+ {v_dst.llvm_ir_optype}\n"
+                )
+
+            diffview.generate_diffview(
+                v_same,
+                v_diff,
+                v_addr_matching,
+                e_con,
+                e_old,
+                e_new,
+                func_name=f,
+                commit_hash=new_hash + "_" + old_hash,
+            )
+
+# Edit distance calculation should include the function symbol.
